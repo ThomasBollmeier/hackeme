e4f543406c39d183c1a6fe9d17cae2b33e84fa53
Thomas Bollmeier <developer@tbollmeier.org>
2018-02-15 22:02:41 +0100

    grammar dev, 2

--- src/hackeme/hackeme_grammar.py
+++ src/hackeme/hackeme_grammar.py
@@ -9,32 +9,87 @@ class HackemeGrammar(Grammar):
         
         Grammar.__init__(self)
         
-        self.g = self
-        
         self._init_tokens()
         self._init_rules()
         
     def _init_tokens(self):
+
+        self.add_keyword('define')
+        self.add_keyword('if')
         
+        self.add_token('LPAR', '\(')
+        self.add_token('RPAR', '\)')
+        
+        self.add_token('IDENT', '[a-z][a-z0-9]*(-[a-z0-9]+)*')
         self.add_token('NUMBER', '\d+')
-    
+        
     def _init_rules(self):
         
         self.rule('start',
-                  OneOf(TokenType('NUMBER')),
+                  Many(
+                    OneOf(
+                     Rule('special_form'),
+                     Rule('expr'))),
                   is_root=True)
-        self.set_ast_transform('start', self._start)
         
-    def _start(self, ast):
-        ret = Ast('hackeme')
-        child = ast.get_children()[0]
-        child.id = ''
-        ret.add_child(child)
-        return ret
-    
+        self.rule('special_form',
+                  OneOf(
+                    Rule('definition'),
+                    Rule('if_expr')))
+        
+        self.rule('definition',
+                  OneOf(
+                    Rule('vardef'),
+                    Rule('fundef')))
+        
+        self.rule('vardef',
+                  Sequence(
+                    TokenType('LPAR'),
+                    TokenType('DEFINE'),
+                    TokenType('IDENT', 'name'),
+                    Rule('expr', 'value'),
+                    TokenType('RPAR')))
+
+        self.rule('fundef',
+                  Sequence(
+                    TokenType('LPAR'),
+                    TokenType('DEFINE'),
+                    TokenType('LPAR'),
+                    TokenType('IDENT', 'name'),
+                    Many(TokenType('IDENT', 'param')),
+                    TokenType('RPAR'),
+                    TokenType('RPAR')))
+        
+        self.rule('if_expr',
+                  Sequence(
+                    TokenType('LPAR'),
+                    TokenType('IF'),
+                    TokenType('RPAR')))
+        
+        self.rule('expr',
+                  OneOf(
+                    Rule('if_expr'),
+                    Rule('call'),
+                    TokenType('IDENT'),
+                    TokenType('NUMBER')))
+
+        self.rule('call',
+                  Sequence(
+                    TokenType('LPAR'),
+                    OneOf(
+                        TokenType('IDENT', 'callee'),
+                        Rule('call', 'callee')),
+                    Many(Rule('expr', 'arg')),
+                    TokenType('RPAR')))
+        
+        
 if __name__ == "__main__":
     
-    code = "42"
+    code = """
+    (define my-answer 42)
+    (define (this-is-a-function a-param))
+    (this-is-a-function 23 some-arg)
+    """
     
     parser = Parser(HackemeGrammar())
     
